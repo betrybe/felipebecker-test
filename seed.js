81601588bc274db4e3412e393e25c5c90158b934
Felipe Becker Nunes <35842441+nunesfb@users.noreply.github.com>
2021-08-12 00:42:07 -0300

    Criacao de uma query em mongo que insira uma pessoa usuária com permissões de admin

--- seed.js
+++ seed.js
@@ -1,1 +1,7 @@
 // colocar query do MongoDB
+const { connection } = require('../models/connection');
+
+const db = await connection();
+await db.users.deleteMany({});
+await db.users.insertOne({ name: 'admin', email: 'root@email.com', password: 'admin', role: 'admin' });
+
